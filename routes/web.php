7e00657e26b1708ea31da4b0468eb9d2700d3754
tzelginianc <tzelginia78@gmail.com>
2022-10-29 13:34:28 +0700

    route

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/dashboard', function () {
+    return view('dashboard.index');
+});
+Route::get('/login', function (){
+    return view(('login'));
+});
+
+Route::get('/register', function () {
+    return view(('register'));
 });
