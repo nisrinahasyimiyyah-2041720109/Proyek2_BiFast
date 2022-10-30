37f37728a47b310755d0b219548040153c5080d5
tzelginianc <tzelginia78@gmail.com>
2022-10-30 18:19:22 +0700

    Login

--- resources/views/layout/login.blade.php
+++ resources/views/layout/login.blade.php
@@ -9,7 +9,7 @@
      {{-- Bootstrp Icons --}}
      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-
+    
     <style>
         body {
           background-image: linear-gradient(rgba(0,0,0,0.7),rgba(0,0,0,0.7)),url(images/login.jpg);	
