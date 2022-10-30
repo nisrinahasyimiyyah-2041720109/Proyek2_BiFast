14f5b17c5cbdf89e4a0566650b42df6f722259dd
tzelginianc <tzelginia78@gmail.com>
2022-10-30 18:07:42 +0700

    LoginController

---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+
+class LoginController extends Controller
+{
+    public function index()
+    {
+        return view('login', [
+            'title' => 'Login'
+        ]);
+    }
+
+    public function authenticate(Request $request)
+    {
+
+        $credentials = $request->validate([
+            'email' => 'required',
+            'password' => 'required'
+        ]);
+
+        if (Auth::attempt($credentials)) {
+            $request->session()->regenerate();
+            if (Auth::user()->verify === 0) {
+                Auth::logout();
+                return back()->with('loginError', 'Login gagal, Silahkan hubungi administrator untuk aktivasi akun anda agar bisa masuk');
+            }
+
+            return redirect()->intended('/dashboard');
+        }
+
+        return back()->with('loginError', 'Login gagal, Silahkan coba lagi');
+    }
+
+    public function logout(Request $request)
+    {
+        Auth::logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerate();
+
+        return redirect('/');
+    }
+
+    public function verify(Request $request, User $user)
+    {
+
+        $user = User::find($request)->first();
+        if ($user) {
+            $user->verify = '1';
+            $user->save();
+        }
+
+        return redirect('/admin/member');
+    }
+
+    public function block(Request $request)
+    {
+
+        $user = User::find($request)->first();
+        if ($user) {
+            $user->verify = '0';
+            $user->save();
+        }
+
+        return redirect('/admin/member');
+    }
+
+}
